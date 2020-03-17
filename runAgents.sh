a45dbe16bd0aec92024b433604f90cb33bed152f
Christian Rieke <rieke@fh-aachen.de>
2020-03-17 09:46:08 +0100

    Start Agents quiet

--- runAgents.sh
+++ runAgents.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 args=("$@")
 echo "Start->"  ${args[0]}
 echo "End->" ${args[1]}
@@ -8,17 +7,17 @@ echo "Typ->" ${args[2]}
 for ((i=$1; i <= $2; i++ ))
         do
                 if [ "${args[2]}" = "RES" ]; then
-                         screen -S RES_$i
+                         screen -dmS RES_$i
                          screen -S RES_$i -X stuff $'python3 dmas/model/agents/res_Agent.py --plz "'$i'" \n'
                  fi
 
                  if [ "${args[2]}" = "DEM" ]; then
-                         screen -S DEM_$i
+                         screen -dmS DEM_$i
                          screen -S DEM_$i -X stuff $'python3 dmas/model/agents/dem_Agent.py --plz "'$i'" \n'
                  fi
 
                  if [ "${args[2]}" = "PWP" ]; then
-                         screen -S PWP_$i
+                         screen -dmS PWP_$i
                          screen -S PWP_$i -X stuff $'python3 dmas/model/agents/pwp_Agent.py --plz "'$i'" \n'
                  fi
 
