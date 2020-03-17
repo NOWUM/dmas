2e94006c0e4209bf393f78d2bec432bf25dcdd4a
Christian Rieke <rieke@fh-aachen.de>
2020-03-17 11:16:35 +0100

    Anspassung der IP-Adressen für Mongo, Influx und den Markt.
    Standard IP: 149.201.150.88 (Multi Core Server)

--- runAgents.sh
+++ runAgents.sh
@@ -2,23 +2,28 @@ args=("$@")
 echo "Start->"  ${args[0]}
 echo "End->" ${args[1]}
 echo "Typ->" ${args[2]}
+echo "Mongo->" ${args[3]}
+echo "Influx->" ${args[4]}
+echo "Market->" ${args[5]}
 
 
 for ((i=$1; i <= $2; i++ ))
+
         do
                 if [ "${args[2]}" = "RES" ]; then
                          screen -dmS RES_$i
-                         screen -S RES_$i -X stuff $'python3 dmas/model/agents/res_Agent.py --plz "'$i'" \n'
-                 fi
+                         screen -S RES_$i -X stuff $'python3 dmas/model/agents/res_Agent.py --plz "'$i'" --mongo "'${args[3]:-149.201.150.88}'"
+                                                                                                         --influx "'${args[4]:-149.201.150.88}'"
+                                                                                                         --market "'${args[5]:-149.201.150.88}'" \n'
+                fi
 
-                 if [ "${args[2]}" = "DEM" ]; then
+                if [ "${args[2]}" = "DEM" ]; then
                          screen -dmS DEM_$i
                          screen -S DEM_$i -X stuff $'python3 dmas/model/agents/dem_Agent.py --plz "'$i'" \n'
-                 fi
+                fi
 
-                 if [ "${args[2]}" = "PWP" ]; then
+                if [ "${args[2]}" = "PWP" ]; then
                          screen -dmS PWP_$i
                          screen -S PWP_$i -X stuff $'python3 dmas/model/agents/pwp_Agent.py --plz "'$i'" \n'
-                 fi
-
+                fi
         done
