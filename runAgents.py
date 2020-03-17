009166ab3fdcc7862c54f8154cbd7098c032ec87
Christian Rieke <rieke@fh-aachen.de>
2020-03-17 06:55:22 +0100

    Start der Agenten auf einem Windows Server mit Hilfe von Python.
    --start --> Start PLZ-Gebiete
    --end --> Ende PLZ - Gebiete
    --typ --> DEM (Verbraucher), RES (Erneuerbare), PWP (Kraftwerke)

---
+++ runAgents.py
@@ -0,0 +1,25 @@
+import os
+os.chdir(os.path.dirname(os.path.dirname(__file__)))
+import argparse
+import subprocess
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--start', type=int, required=False, default=1, help='Start-Point to build Areas')
+    parser.add_argument('--end', type=int, required=False, default=2, help='End-Point to build Areas')
+    parser.add_argument('--typ', type=str, required=False, default='PWP', help='Agent Typ like RES, PWP or DEM')
+
+    return parser.parse_args()
+
+if __name__=="__main__":
+
+    path = os.getcwd()
+    args = parse_args()
+
+    for i in range(args.start, args.end +1):
+        if args.typ == 'RES':
+            subprocess.Popen('python ' + path + r'/agents/res_Agent.py ' + '--plz %i' % i, cwd=path, shell=True)
+        elif args.typ == 'DEM':
+            subprocess.Popen('python ' + path + r'/agents/dem_Agent.py ' + '--plz %i' % i, cwd=path, shell=True)
+        elif args.typ == 'PWP':
+            subprocess.Popen('python ' + path + r'/agents/pwp_Agent.py ' + '--plz %i' % i, cwd=path, shell=True)
