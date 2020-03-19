f7e3e7a87a0cb8077a42f81693e556413881a68b
Christian Rieke <rieke@fh-aachen.de>
2020-03-19 14:34:08 +0000

    Hotfix frcst_Price.py

--- model/apps/frcst_Price.py
+++ model/apps/frcst_Price.py
@@ -5,8 +5,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from keras import models, layers, optimizers
 
-
-class priceForecast:
+class demandForecast:
 
     def __init__(self, influx):
 
@@ -16,15 +15,15 @@ class priceForecast:
 
         self.scaler = StandardScaler()
         self.model = models.Sequential()
-        self.model.add(layers.Dense(120, activation='sigmoid', input_shape=(52,)))
+        self.model.add(layers.Dense(120, activation='sigmoid', input_shape=(49,)))
         self.model.add(layers.Dense(120, activation='relu'))
         self.model.add(layers.Dense(1, activation='linear'))
         self.model.compile(loss='mean_absolute_percentage_error', optimizer=opt, metrics=['mean_squared_error'])
 
-        self.input = dict(GHI=[], Ws=[], TAmb=[], demand=[], dummies=[])
-        self.inputArchiv = dict(GHI=[], Ws=[], TAmb=[], demand=[], dummies=[])
-        self.mcp = []
-        self.mcpArchiv = []
+        self.input = dict(GHI=[], Ws=[], TAmb=[], dummies=[])
+        self.inputArchiv = dict(GHI=[], Ws=[], TAmb=[], dummies=[])
+        self.demand = []
+        self.demandArchiv = []
 
         self.fitted = False
         self.collect = 10
@@ -55,52 +54,41 @@ class priceForecast:
         self.input['TAmb'].append(np.asarray(lst[0]).reshape((-1, 1)))
         self.input['GHI'].append(np.asarray(lst[1]).reshape((-1, 1)))
         self.input['Ws'].append(np.asarray(lst[2]).reshape((-1, 1)))
-
-        query = 'SELECT sum("P") FROM "DEM" WHERE time >= \'%s\' and time < \'%s\' GROUP BY time(1h) fill(null)' % (start, end)
+        query = 'SELECT sum("Power") FROM "Areas" WHERE time >= \'%s\' and time < \'%s\'  and "timestamp" = \'optimize_dayAhead\' GROUP BY time(1h) fill(0)' % (start, end)
         result = self.influx.query(query)
         demand = [np.round(point['sum'],2) for point in result.get_points()]
 
-        self.input['demand'].append(np.asarray(demand).reshape((-1,1)))
-
-        query = 'SELECT sum("price") FROM "DayAhead" WHERE time >= \'%s\' and time < \'%s\' GROUP BY time(1h) fill(null)' % (start, end)
-        result = self.influx.query(query)
-        mcp = [np.round(point['sum'],2) for point in result.get_points()]
-
-        self.mcp.append(np.asarray(mcp).reshape((-1,1)))
+        self.demand.append(np.asarray(demand).reshape((-1,1)))
 
     def fitFunction(self):
+        X = np.asarray(self.input['TAmb']).reshape((-1, 1))
 
         if not self.fitted:
+            self.scaler.fit(X)
             self.fitted = True
-
-        demand = np.asarray(self.input['demand']).reshape((-1, 1))
-        TAmb = np.asarray(self.input['TAmb']).reshape((-1, 1))
-        Ws = np.asarray(self.input['Ws']).reshape((-1, 1))
-        Ghi = np.asarray(self.input['GHI']).reshape((-1, 1))
-
-        X = np.concatenate((demand, TAmb, Ws, Ghi), axis=1)
 
         self.scaler.partial_fit(X)
         Xstd = self.scaler.transform(X)
-        dummies = np.asarray(self.input['dummies']).reshape((-1, 48))
+        dummies = np.asarray(self.input['dummies']).reshape((-1,48))
         Xstd = np.concatenate((Xstd, dummies), axis=1)
 
-        y = np.asarray(self.mcp).reshape((-1, 1))
+        y = np.asarray(self.demand).reshape((-1, 1))
 
         X_train, X_test, y_train, y_test = train_test_split(Xstd, y, test_size=0.35)
 
         self.model.fit(X_train, y_train, epochs=200, batch_size=15, validation_data=(X_test, y_test))
 
-    def forecast(self, date, demand):
+    def forecast(self, date):
 
         if self.fitted:
-            # -- create start and end date for query and dummies
+                    # -- create start and end date for query and dummies
             start = date.isoformat() + 'Z'
             end = (date + pd.DateOffset(days=1)).isoformat() + 'Z'
 
             # -- create dummies for whole year and select the corresponding day
             dummies = createSaisonDummy(pd.to_datetime('%i-01-01' % date.year),
-                                        pd.to_datetime('%i-01-01' % (date + pd.DateOffset(years=1)).year), hour=True)
+                                        pd.to_datetime('%i-01-01' % (date + pd.DateOffset(years=1)).year),
+                                        hour=True)
             index = pd.date_range(start=date, periods=24, freq='h')
             dummies = dummies.loc[index, :].to_numpy()
 
@@ -112,25 +100,11 @@ class priceForecast:
                 result = self.influx.query(query)
                 lst.append([point['mean'] for point in result.get_points()])
 
-            demand = np.asarray(demand).reshape((-1,1))
             TAmb = np.asarray(lst[0]).reshape((-1, 1))
-            Ghi = np.asarray(lst[1]).reshape((-1, 1))
-            Ws = np.asarray(lst[2]).reshape((-1, 1))
-
-            X = np.concatenate((demand.reshape(-1, 1), TAmb.reshape(-1, 1), Ws.reshape(-1, 1), Ghi.reshape(-1, 1)), axis=1)
-            Xstd = self.scaler.transform(X)
-            Xstd = np.concatenate((Xstd, dummies), axis=1)
-            price = self.model.predict(Xstd)
-
-            power_price = price.reshape((-1,))                                              # -- Power Price        [€/MWh]
+            Xstd = np.concatenate((self.scaler.transform(TAmb), dummies), axis=1)
+            demand = self.model.predict(Xstd)
 
         else:
-            power_price = 25*np.ones(24)
+            demand = 10000 * np.zeros(24)
 
-        co = np.ones_like(power_price) * 20                                                 # -- Emission Price     [€/MWh]
-        gas = np.ones_like(power_price) * 3                                                 # -- Gas Price          [€/MWh]
-        lignite = 1.5                                                                       # -- Lignite Price      [€/MWh]
-        coal = 2                                                                            # -- Hard Coal Price    [€/MWh]
-        nuc = 1                                                                             # -- nuclear Price      [€/MWh]
-
-        return dict(power=power_price, gas=gas, co=co, lignite=lignite, coal=coal, nuc=nuc)
+        return demand
