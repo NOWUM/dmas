ce7325383482b2bf32f08e484c4d9066f08c3cad
Christian Rieke <rieke@fh-aachen.de>
2020-03-24 12:57:08 +0000

    Update res_Wind.py

--- model/components/res_Wind.py
+++ model/components/res_Wind.py
@@ -9,14 +9,19 @@ class wind_model(es_model):
                  factors=np.asarray(np.load(open(r'./data/Ref_Factors.array', 'rb')), np.float32),
                  parameters=np.asarray([2.8, -37, 5.4, 0.17], np.float32)):
         super().__init__(t, T, dt, demQ, refSLP, refTemp, factors, parameters)
-        z0 = 0.5
-        self.factor = (np.log(110 / z0) / np.log(2 / z0))
 
     def build(self, data, timeseries, date):
         day = []
+        z0 = 0.5
+        if data['P'] <= 600:
+            factor = (np.log(50 / z0) / np.log(2 / z0))
+        elif data['P'] <= 3000:
+            factor = (np.log(95 / z0) / np.log(2 / z0))
+        else:
+            factor = (np.log(125 / z0) / np.log(2 / z0))
 
         for wind in timeseries['wind']:
-            wind = wind * self.factor
+            wind = wind * factor
             if wind <= data['take_off']:
                 generation = 0
             elif wind >= data['cut_off']:
