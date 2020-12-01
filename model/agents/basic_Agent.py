f8765a8560ab5945b42f7e1aca0b50dac715ef93
Rieke <Rieke@fh-aachen.de>
2020-12-01 08:04:01 +0100

    - add print statements - changes in price forecast to get a better forecast score - save last 1000 Results to train the ann

--- model/agents/basic_Agent.py
+++ model/agents/basic_Agent.py
@@ -89,7 +89,7 @@ class agent:
 
         mcp = [37.70, 35.30, 33.90, 33.01, 33.27, 35.78, 43.17, 50.21, 52.89, 51.18, 48.24, 46.72, 44.23,
                42.29, 41.60, 43.12, 45.37, 50.95, 55.12, 56.34, 52.70, 48.20, 45.69, 40.25]
-        self.week_price_list = WeekPriceList(defaultDay=mcp)
+        self.week_price_list = WeekPriceList(defaultDay=np.asarray(mcp))
 
         # forecast methods
         self.forecasts = {
