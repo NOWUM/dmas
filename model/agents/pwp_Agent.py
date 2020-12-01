ee86c8cd8a1b55a99636104f21355df70c255950
Rieke <Rieke@fh-aachen.de>
2020-12-01 07:45:57 +0100

    - add print statements - changes in price forecast to get a better forecast score - save last 1000 Results to train the ann

--- model/agents/pwp_Agent.py
+++ model/agents/pwp_Agent.py
@@ -428,16 +428,16 @@ if __name__ == "__main__":
     # from matplotlib import pyplot as plt
 
     args = parse_args()
-    agent = PwpAgent(date='2018-01-01',  plz=40) #plz=args.plz)
-    # agent.connections['mongoDB'].login(agent.name)
-    # try:
-    #     agent.run()
-    # except Exception as e:
-    #     print(e)
-    # finally:
-    #     agent.connections['mongoDB'].logout(agent.name)
-    #     agent.connections['influxDB'].influx.close()
-    #     agent.connections['mongoDB'].mongo.close()
-    #     if not agent.connections['connectionMQTT'].is_closed:
-    #         agent.connections['connectionMQTT'].close()
-    #     exit()
+    agent = PwpAgent(date='2018-01-01', plz=args.plz)
+    agent.connections['mongoDB'].login(agent.name)
+    try:
+        agent.run()
+    except Exception as e:
+        print(e)
+    finally:
+        agent.connections['mongoDB'].logout(agent.name)
+        agent.connections['influxDB'].influx.close()
+        agent.connections['mongoDB'].mongo.close()
+        if not agent.connections['connectionMQTT'].is_closed:
+            agent.connections['connectionMQTT'].close()
+        exit()
