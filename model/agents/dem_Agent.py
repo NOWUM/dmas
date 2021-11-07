89ed41d95a601d550e112ab6991be77d04a25785
christian <D4e85m6n!131345>
2021-11-07 14:16:48 +0100

    Build DEM-Agent Image

--- model/agents/dem_Agent.py
+++ model/agents/dem_Agent.py
@@ -7,7 +7,6 @@ import pandas as pd
 import numpy as np
 
 # model modules
-os.chdir(os.path.dirname(os.path.dirname(__file__)))
 from aggregation.portfolio_demand import DemandPortfolio
 from agents.client_Agent import agent as basicAgent
 
