7c7b7ad6d2989e954dbbf4ac9d9ebbda77d66604
Christian Rieke <rieke@fh-aachen.de>
2021-11-17 10:35:10 +0100

    update DWD-Crawler

--- model/main_dwd.py
+++ model/main_dwd.py
@@ -6,4 +6,4 @@ logging.basicConfig()
 
 if __name__ == "__main__":
     crawler = OpenDWDCrawler()
-    #crawler.get_data(start='199501', end='201601')
+    crawler.get_data(start='199501', end='201601')
