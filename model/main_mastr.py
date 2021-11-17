f39fc79345e6fac889ae1cef21f5ecf86005ab84
Christian Rieke <rieke@fh-aachen.de>
2021-11-17 10:41:39 +0100

    update DWD-Crawler

--- model/main_mastr.py
+++ model/main_mastr.py
@@ -6,11 +6,10 @@ if __name__ == "__main__":
 
     engine = create_engine('postgresql://opendata:opendata@10.13.10.41:5432')
 
-    while True:
-        init_database(connection=engine)
-        mastr_data = get_data_from_MaStR()
-        create_db_from_export(connection=engine)
+    # while True:
+    init_database(connection=engine)
+    create_db_from_export(connection=engine)
 
-        time.sleep(2*(60*60*24))
+    # time.sleep(2*(60*60*24))
 
 
