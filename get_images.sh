cf25645408e95e370e22abb99d5a3bfc59f42740
Christian Rieke <rieke@fh-aachen.de>
2021-11-23 14:16:35 +0100

    changes for repo registry in git

---
+++ get_images.sh
@@ -0,0 +1,6 @@
+docker pull registry.git.fh-aachen.de/nowum-energy/dmas/pwp_agent:latest
+docker pull registry.git.fh-aachen.de/nowum-energy/dmas/mrk_agent:latest
+docker pull registry.git.fh-aachen.de/nowum-energy/dmas/net_agent:latest
+docker pull registry.git.fh-aachen.de/nowum-energy/dmas/res_agent:latest
+docker pull registry.git.fh-aachen.de/nowum-energy/dmas/dem_agent:latest
+docker pull registry.git.fh-aachen.de/nowum-energy/dmas/wtr_agent:latest
