0a1c0c0ac8a52ee79f056d4a50f40f41d17eb9a8
Christian Rieke <rieke@fh-aachen.de>
2021-12-03 08:25:42 +0100

    changes in dockerfiles

--- model/main.py
+++ model/main.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
     init_dict = {
         'date': os.getenv('SIMULATION_START_DATE', '2018-01-01'),
-        'plz': os.getenv('PLZ_CODE', 52),
+        'plz': int(os.getenv('PLZ_CODE', 52)),
         'mqtt_exchange': os.getenv('MQTT_EXCHANGE', 'dMAS'),
         'simulation_database': os.getenv('SIMULATIONS_DATABASE', 'dMAS'),
         'agent_type': os.getenv('AGENT_TYPE', 'DEM'),
