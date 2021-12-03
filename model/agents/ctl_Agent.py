ca0648d40e82bb22d0dc2356f915e3fdb1593f03
Christian Rieke <rieke@fh-aachen.de>
2021-12-03 10:49:03 +0100

    changes in systems

--- model/agents/ctl_Agent.py
+++ model/agents/ctl_Agent.py
@@ -7,6 +7,7 @@ import os
 # model modules
 from agents.basic_Agent import BasicAgent
 
+
 class CtlAgent(BasicAgent):
 
     def __init__(self, date, plz, agent_type, mqtt_exchange, connect,  infrastructure_source, infrastructure_login):
@@ -17,37 +18,38 @@ class CtlAgent(BasicAgent):
 
     def run(self):
 
-        for date in pd.date_range(start=self.start, end=self.end, freq='D'):
+        # for date in pd.date_range(start=self.start, end=self.end, freq='D'):
 
             try:
-                start_time = tme.time()  # timestamp to measure simulation time
-
-                # 1.Step: Run optimization for dayAhead Market
-                self.channel.basic_publish(exchange=self.exchange, routing_key='',
-                                           body='opt_dayAhead ' + str(date))
-                # 2. Step: Run Market Clearing
-                self.channel.basic_publish(exchange=self.exchange, routing_key='',
-                                           body='dayAhead_clearing ' + str(date))
-
-                # TODO: Changes for wtr Agent
-                # weather_generator.generate_weather(valid=valid, date=pd.to_datetime(start))
-                # while not m_con.get_market_status(date):  # check if clearing done
-                #     if gen_weather:
-                #         weather_generator.generate_weather(valid, date + pd.DateOffset(days=1))
-                #         gen_weather = False
-                #     else:
-                #         tme.sleep(1)
-                gen_weather = True
-
-                # 3. Step: Run Power Flow calculation
-                self.channel.basic_publish(exchange=self.exchange, routing_key='',
-                                           body='grid_calc ' + str(date))
-                # 4. Step: Publish Market Results
-                self.channel.basic_publish(exchange=self.exchange, routing_key='',
-                                           body='result_dayAhead ' + str(date))
-
-                end_time = tme.time() - start_time
-                self.logger.info('Day %s complete in: %s seconds ' % (str(date.date()), end_time))
+                pass
+                # start_time = tme.time()  # timestamp to measure simulation time
+                #
+                # # 1.Step: Run optimization for dayAhead Market
+                # self.channel.basic_publish(exchange=self.exchange, routing_key='',
+                #                            body='opt_dayAhead ' + str(date))
+                # # 2. Step: Run Market Clearing
+                # self.channel.basic_publish(exchange=self.exchange, routing_key='',
+                #                            body='dayAhead_clearing ' + str(date))
+                #
+                # # TODO: Changes for wtr Agent
+                # # weather_generator.generate_weather(valid=valid, date=pd.to_datetime(start))
+                # # while not m_con.get_market_status(date):  # check if clearing done
+                # #     if gen_weather:
+                # #         weather_generator.generate_weather(valid, date + pd.DateOffset(days=1))
+                # #         gen_weather = False
+                # #     else:
+                # #         tme.sleep(1)
+                # gen_weather = True
+                #
+                # # 3. Step: Run Power Flow calculation
+                # self.channel.basic_publish(exchange=self.exchange, routing_key='',
+                #                            body='grid_calc ' + str(date))
+                # # 4. Step: Publish Market Results
+                # self.channel.basic_publish(exchange=self.exchange, routing_key='',
+                #                            body='result_dayAhead ' + str(date))
+                #
+                # end_time = tme.time() - start_time
+                # self.logger.info('Day %s complete in: %s seconds ' % (str(date.date()), end_time))
 
             except Exception as e:
                 print('Error ' + str(date.date()))
