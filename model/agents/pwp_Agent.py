fixes for pwp - fixed units - copy price dataframe between solved steps - fix condition for minimal start and stop time - fixed emission factor in structure interface - configurable steps in powerplant portfolio

model/agents/pwp_Agent.py
@@ -65,7 +65,7 @@ class PwpAgent(BasicAgent):
             prevent_orders = {}
 
             # build orders for each offset
-            for offset in [-10, -5, 0, 5, 100]:
+            for offset in self.portfolio.steps:
                 # get optimization result for key (block) and offset
                 result = model.optimization_results[offset]
                 starts = model.prevented_start[offset]
@@ -193,7 +193,7 @@ class PwpAgent(BasicAgent):
                                 price = np.round(
                                     (result['fuel'][hour] + result['emission'][hour]) / result['power'][hour], 2)
                                 power = np.round(0.2 * delta[hour], 2)
-                                # split volume in five orders and add them to order_boo
+                                # split volume in five orders and add them to order_book
                                 for order in range(5):
                                     order_book.update({(block_number, hour, order, name): (price,
                                                                                            power,

model/aggregation/portfolio_powerPlant.py
@@ -14,12 +14,13 @@ log.setLevel('INFO')
 
 class PowerPlantPortfolio(PortfolioModel):
 
-    def __init__(self, T=24, date='2020-01-01'):
+    def __init__(self, T=24, date='2020-01-01', steps=np.array([-10, -5, 0, 5, 100])/1e3):
         super().__init__(T, date)
+        self.steps = steps
 
 
     def add_energy_system(self, energy_system):
-        model = PowerPlant(T=self.T, steps=[-10, -5, 0, 5, 100], **energy_system)
+        model = PowerPlant(T=self.T, steps=self.steps, **energy_system)
         self.capacities[str(energy_system['fuel']).replace('_combined', '')] += energy_system['maxPower']/1e3 # [kW] -> [MW]
         self.energy_systems.append(model)
 

model/forecasts/price.py
@@ -13,13 +13,15 @@ from forecasts.demand import DemandForecast
 from forecasts.weather import WeatherForecast
 
 
-# load default prices in €/MWh therm -> convert to €/kWh therm
 with open(r'./forecasts/data/default_price.pkl', 'rb') as file:
+    # load default prices in €/MWh elek -> convert to €/kWh
     default_power_price = np.load(file).reshape((24,))/1e3                  # hourly mean values 2015-2018
 with open(r'./forecasts/data/default_gas.pkl', 'rb') as file:
+    # load default prices in €/MWh therm -> convert to €/kWh therm
     default_gas = np.load(file).reshape((12,))/1e3                          # month mean values year 2018
 with open(r'./forecasts/data/default_emission.pkl', 'rb') as file:
-    default_emission = np.load(file).reshape((12,))/1e3                     # month mean values year 2018
+    # load default prices in €/t CO2  -> convert to €/t CO2
+    default_emission = np.load(file).reshape((12,))                         # month mean values year 2018
 
 default_coal = 65.18 / 8.141 /1e3                                           # €/ske --> €/MWh -> €/kWh
 default_lignite = 1.5 /1e3                                                  # agora Deutsche "Braunkohlewirtschaft" in €/kWh

model/interfaces/structure.py
@@ -154,17 +154,20 @@ class InfrastructureInterface:
                     # Set technical parameter corresponding to the type (0, 2000, 2018)
                     technical_parameter = pd.read_csv(fr'./interfaces/data/technical_parameter_{fuel_type}.csv',
                                                       sep=';', decimal=',', index_col=0)
+                    # start cost given in [€/MW]
+                    # chi in [t CO2/MWh therm.]
 
                     for line, row in df.iterrows():
+                        start_cost = technical_parameter.at[row['type'], 'startCost']/1e3 # [€/MW] -> [€/kW]
                         df.at[line, 'minPower'] = df.at[line, 'maxPower'] * technical_parameter.at[row['type'], 'minPower'] / 100
                         df.at[line, 'P0'] = df.at[line, 'minPower']
                         df.at[line, 'gradP'] = np.round(df.at[line, 'maxPower'] * technical_parameter.at[row['type'], 'gradP'] * 60 / 100,2)
                         df.at[line, 'gradM'] = np.round(df.at[line, 'maxPower'] * technical_parameter.at[row['type'], 'gradM'] * 60 / 100,2)
                         df.at[line, 'eta'] = technical_parameter.at[row['type'], 'eta']
-                        df.at[line, 'chi'] = technical_parameter.at[row['type'], 'chi']
+                        df.at[line, 'chi'] = technical_parameter.at[row['type'], 'chi']/1e3 # [t CO2/MWh therm.] -> [t CO2/kWh therm.]
                         df.at[line, 'stopTime'] = technical_parameter.at[row['type'], 'stopTime']
                         df.at[line, 'runTime'] = technical_parameter.at[row['type'], 'runTime']
-                        df.at[line, 'startCost'] = df.at[line, 'maxPower'] * technical_parameter.at[row['type'], 'startCost']
+                        df.at[line, 'startCost'] = df.at[line, 'maxPower'] * start_cost
 
                     data_frames.append(df)
 

model/systems/generation_powerPlant.py
@@ -17,11 +17,10 @@ class PowerPlant(EnergySystem):
 
         self.name = unitID
 
-        # PWP solves in MW but is initialized with kW as all others too
-        # emission factor chi is given in t/MWh
-        self.power_plant = dict(fuel=fuel, maxPower=maxPower, minPower=minPower, eta=eta, P0=P0, chi=chi/1e3, 
+        # PWP solves in kW as all others too
+        self.power_plant = dict(fuel=fuel, maxPower=maxPower, minPower=minPower, eta=eta, P0=P0, chi=chi,
                                 stopTime=stopTime, runTime=runTime, gradP=gradP, gradM=gradM, on=on, off=off)
-        self.start_cost = startCost/1e3 # [€/kW] -> [€/MW]
+        self.start_cost = startCost
 
         self.model = ConcreteModel()
         self.opt = SolverFactory('glpk')
@@ -34,6 +33,7 @@ class PowerPlant(EnergySystem):
                                                 emission=np.zeros(self.T, float),
                                                 fuel=np.zeros(self.T, float),
                                                 start=np.zeros(self.T, float),
+                                                profit=np.zeros(self.T, float),
                                                 obj=0) for step in steps}
 
         self.prevented_start = {step: dict(prevent_start=False,
@@ -57,6 +57,7 @@ class PowerPlant(EnergySystem):
         self.model.w = Var(self.t, within=Binary)
 
         # cash flow variables
+        # XXX not opt variable - can be pyomo expression
         self.model.fuel = Var(self.t, bounds=(0, None), within=Reals)
         self.model.emissions = Var(self.t, bounds=(0, None), within=Reals)
         self.model.start_ups = Var(self.t, bounds=(0, None), within=Reals)
@@ -76,7 +77,7 @@ class PowerPlant(EnergySystem):
         self.model.run_time = ConstraintList()
         self.model.states = ConstraintList()
         self.model.initial_on = ConstraintList()
-        self.model.initial_off =ConstraintList()
+        self.model.initial_off = ConstraintList()
         # define constraint for cash-flow aspects
         self.model.fuel_cost = ConstraintList()
         self.model.emission_cost = ConstraintList()
@@ -93,7 +94,7 @@ class PowerPlant(EnergySystem):
             # output power of the plant
             self.model.real_power.add(self.model.p_out[t] == self.model.p_model[t] + self.model.z[t]
                                       * self.power_plant['minPower'])
-            if t < 23:
+            if t < 23: # only the next day
                 self.model.real_max.add(self.model.p_out[t] <= self.power_plant['minPower']
                                         * (self.model.z[t] + self.model.v[t+1] + self.model.p_model[t]))
             # model power for optimization
@@ -122,7 +123,7 @@ class PowerPlant(EnergySystem):
 
             if t < self.power_plant['runTime'] - self.power_plant['on']:
                 self.model.initial_on.add(self.model.z[t] == 1)
-            elif t < self.power_plant['stopTime'] - self.power_plant['off']:
+            elif self.power_plant['off']>0 and t < self.power_plant['stopTime'] - self.power_plant['off']:
                 self.model.initial_off.add(self.model.z[t] == 0)
 
             self.model.fuel_cost.add(self.model.fuel[t]
@@ -171,9 +172,9 @@ class PowerPlant(EnergySystem):
 
         if self.committed_power is None:
 
-            base_price = self.prices.loc[:, 'power']
-            prices_24h = self.prices.iloc[:24, :]
-            prices_48h = self.prices.iloc[:48, :]
+            base_price = self.prices.loc[:, 'power'].copy()
+            prices_24h = self.prices.iloc[:24, :].copy()
+            prices_48h = self.prices.iloc[:48, :].copy()
 
             for step in self.steps:
 
@@ -189,6 +190,7 @@ class PowerPlant(EnergySystem):
                     self.optimization_results[step]['fuel'][t] = self.model.fuel[t].value
                     self.optimization_results[step]['start'][t] = self.model.start_ups[t].value
                     self.optimization_results[step]['obj'] = value(self.model.obj)
+                    self.optimization_results[step]['profit'][t] = value(self.model.profit[t].value)
 
                 p_out = np.asarray([self.model.p_out[t].value for t in self.t])
                 objective_value = value(self.model.obj)
@@ -211,7 +213,8 @@ class PowerPlant(EnergySystem):
                 if step == 0:
                     self.cash_flow = dict(fuel=self.optimization_results[step]['fuel'],
                                           emission=self.optimization_results[step]['emission'],
-                                          start_ups=self.optimization_results[step]['start'])
+                                          start_ups=self.optimization_results[step]['start'],
+                                          profit=self.optimization_results[step]['profit'])
                     self.generation[str(self.power_plant['fuel']).replace('_combined', '')] = self.optimization_results[step]['power']
                     self.power = self.optimization_results[step]['power']
 
@@ -222,6 +225,7 @@ class PowerPlant(EnergySystem):
                 self.cash_flow['fuel'][t] = float(self.model.fuel[t].value)
                 self.cash_flow['emission'][t] = float(self.model.emissions[t].value)
                 self.cash_flow['start_ups'][t] = float(self.model.start_ups[t].value)
+                self.cash_flow['profit'][t] = float(self.model.profit[t].value)
                 self.power[t] = float(self.model.p_out[t].value)
                 self.generation[str(self.power_plant['fuel']).replace('_combined', '')][t] = self.power[t]
 
@@ -232,27 +236,28 @@ class PowerPlant(EnergySystem):
 if __name__ == "__main__":
     plant = {'unitID':'x',
              'fuel':'lignite',
-             'maxPower': 300,
-             'minPower': 100,
-             'eta': 0.4,
+             'maxPower': 300, # kW
+             'minPower': 100, # kW
+             'eta': 0.4, # Wirkungsgrad
              'P0': 120,
-             'chi': 0.407,
-             'stopTime': 5,
-             'runTime': 10,
-             'gradP': 300,
-             'gradM': 300,
-             'on': 1,
+             'chi': 0.407/1e3, # t CO2/kWh
+             'stopTime': 5, # hours
+             'runTime': 10, # hours
+             'gradP': 300, # kW/h
+             'gradM': 300, # kW/h
+             'on': 1, # running since
              'off': 0,
-             'startCost': 10*1e3}
+             'startCost': 1e3 # €/Start
+             }
+    steps = np.array([-100, -10, 0, 10, 100])
+    pw = PowerPlant(T=24, steps=steps, **plant)
 
-    pw = PowerPlant(T=24, steps=[-100, -10, 0, 10, 1000], **plant)
-
-    power_price = np.ones(48) * 27 * np.random.uniform(0.95, 1.05, 48)
-    co = np.ones(48) * 23.8 * np.random.uniform(0.95, 1.05, 48)     # -- Emission Price     [€/t]
-    gas = np.ones(48) * 24.8 * np.random.uniform(0.95, 1.05, 48)    # -- Gas Price          [€/MWh]
-    lignite =np.ones(48) * 1.5 * np.random.uniform(0.95, 1.05)                   # -- Lignite Price      [€/MWh]
-    coal = np.ones(48) * 9.9 * np.random.uniform(0.95, 1.05)                      # -- Hard Coal Price    [€/MWh]
-    nuc = np.ones(48) * 1.0 * np.random.uniform(0.95, 1.05)                       # -- nuclear Price      [€/MWh]
+    power_price = np.ones(48) * 0.3 #* np.random.uniform(0.95, 1.05, 48) # €/kWh
+    co = np.ones(48) * 23.8 #* np.random.uniform(0.95, 1.05, 48)     # -- Emission Price     [€/t]
+    gas = np.ones(48) * 0.03 #* np.random.uniform(0.95, 1.05, 48)    # -- Gas Price          [€/kWh]
+    lignite =np.ones(48) * 0.015 #* np.random.uniform(0.95, 1.05)                   # -- Lignite Price      [€/kWh]
+    coal = np.ones(48) * 0.02 #* np.random.uniform(0.95, 1.05)                      # -- Hard Coal Price    [€/kWh]
+    nuc = np.ones(48) * 0.01 #* np.random.uniform(0.95, 1.05)                       # -- nuclear Price      [€/kWh]
 
     prices = dict(power=power_price, gas=gas, co=co, lignite=lignite, coal=coal, nuc=nuc)
     prices = pd.DataFrame(data=prices, index=pd.date_range(start='2018-01-01', freq='h', periods=48))
@@ -260,9 +265,15 @@ if __name__ == "__main__":
                      prices=prices)
 
     power = pw.optimize()
+    test_power = power.copy()
 
-    power *= 0.5
+    clean_spread = (1/0.4 * 0.015 + 1/0.4 * 0.407/1e3 * 23.8)
+    plant['maxPower']* clean_spread # €/kWh cost
 
-    pw.committed_power = power
+    # assume market only gives you halve of your offers
+    pw.committed_power = power/2
     pw.build_model()
     pw.optimize()
+
+    assert all(test_power/2 == pw.power)
+    # actual schedule corresponds to the market result