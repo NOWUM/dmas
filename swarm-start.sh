1508c9eaab243caf47157863863ee9da6fe21643
Florian Maurer <maurer@fh-aachen.de>
2022-08-11 21:31:30 +0200

    fix timescaledb join query https://stackoverflow.com/a/69760804

--- swarm-start.sh
+++ swarm-start.sh
@@ -25,7 +25,12 @@ docker service rm dmas_res_de941
 
 docker service rm dmas_dem_de91c
 
-CONTAINER_COUNT=`echo $(docker stack ps dmas | grep Running | wc -l) - 5 | bc`
+docker service rm dmas_dem_deb38
+docker service rm dmas_res_deb38
+
+sleep 60;
+
+CONTAINER_COUNT=`echo $(docker stack ps dmas | grep Running | wc -l) - 7 | bc`
 while test " $(curl -s http://localhost:5000/agent_count)" -lt $CONTAINER_COUNT
 do
   sleep 7;
@@ -34,6 +39,6 @@ done
 
 sleep 10
 
-curl -X POST http://localhost:5000/start -d "begin=2018-01-01" -d "end=2018-02-01"
+curl -X POST http://localhost:5000/start -d "begin=2018-01-01" -d "end=2018-04-01"
 curl -X POST http://localhost:5000/wait_limit -d "wait_limit=900"
 echo ""
