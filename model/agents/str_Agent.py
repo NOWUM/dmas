d83725150e6c627076ae1d72c7f40c50871e8f89
Florian Maurer <maurer@fh-aachen.de>
2023-01-06 02:14:16 +0100

    use historic data for training

--- model/agents/str_Agent.py
+++ model/agents/str_Agent.py
@@ -22,7 +22,7 @@ class StrAgent(BasicAgent):
 
         self.weather_forecast = WeatherForecast(position=dict(lat=self.latitude, lon=self.longitude),
                                                 weather_interface=self.weather_interface)
-        self.price_forecast = PriceForecast(use_historic_data=False, use_real_data=False)
+        self.price_forecast = PriceForecast(use_historic_data=True, use_real_data=False)
         self.demand_forecast = DemandForecast()
 
         self.storage_names = []
